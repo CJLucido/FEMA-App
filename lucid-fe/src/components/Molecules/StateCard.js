e6e2332b9e7a45e7bed86e13dce7e6e7d0e4a34e
Carlo <c.jose.lucido@gmail.com>
2020-06-24 12:36:31 -0700

    state card edited to show information desired after state is chosen

--- lucid-fe/src/components/Molecules/StateCard.js
+++ lucid-fe/src/components/Molecules/StateCard.js
@@ -3,33 +3,32 @@ import {Card} from 'antd';
 
 export default function StateCard(props) {
  
-    const gridStyle = {
-        width: '33%',
+    const gridStyle1 = {
+        width: '50%',
         textAlign: 'center',
     }
 
+    const gridStyle2 = {
+        width: '33.33333333%',
+        textAlign: 'center',
+
+    }
+
   return (
-    <Card style={{margin:"2em", backgroundColor:"gray", fontSize:".5em"}} title={props.stateName}>
-        <Card.Grid style={gridStyle}>
-        {props.stateName}
+    <Card style={{margin:"2em",backgroundColor:"#000025", borderRadius:"00px", boxShadow:"0px 4px 4px rgba(0,0,0,0.25)"}}>
+    <Card style={{ backgroundColor:"gray", fontSize:"1em", borderRadius:"00px"}} title={props.disasterType + " " + props.disasterNumber + " " + props.titleDR}>
+        <Card.Grid style={gridStyle1}>
+        Began: {props.beganDate ? props.beganDate.slice(0,10) : props.beganDate}
         </Card.Grid>
-        <Card.Grid style={gridStyle}>
-        {props.titleDR}
+        <Card.Grid style={gridStyle1}>
+        Closed: {props.closeDate ? props.closeDate.slice(0,10) : props.closeDate}
         </Card.Grid>
-        <Card.Grid style={gridStyle}>
-        {props.disasterType + " " + props.disasterNumber}
-        </Card.Grid>
-        <Card.Grid style={gridStyle}>
-        Began: {props.beganDate}
-        </Card.Grid>
-        <Card.Grid style={gridStyle}>
-        Closed: {props.closeDate}
-        </Card.Grid>
-        <Card.Grid style={gridStyle}>{props.paProgramDeclared ? "PA Support" : "No PA Support"}</Card.Grid>
-        <Card.Grid style={gridStyle}>{props.iaProgramDeclared ? "IA Support" : "No IA Support"}</Card.Grid>
-        <Card.Grid style={gridStyle}>{props.hmProgramDeclared ? "HM Support" : "No HM Support"}</Card.Grid>
+        <Card.Grid style={gridStyle2}>{props.paProgramDeclared ? "PA Support" : "No PA Support"}</Card.Grid>
+        <Card.Grid style={gridStyle2}>{props.iaProgramDeclared ? "IA Support" : "No IA Support"}</Card.Grid>
+        <Card.Grid style={gridStyle2}>{props.hmProgramDeclared ? "HM Support" : "No HM Support"}</Card.Grid>
 
    
+    </Card>
     </Card>
   );
 }
